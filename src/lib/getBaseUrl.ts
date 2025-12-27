994f4ce1bb12a6944cecaffac818fb9d708d874d
Omair Iqbal <omair.iqbal@ibm.com>
2025-12-27 15:52:51 +0000

    Updated getBaseUrl

--- src/lib/getBaseUrl.ts
+++ src/lib/getBaseUrl.ts
@@ -1,10 +1,17 @@
-function getBaseUrl() {
+function getBaseUrl(): string {
   if (process.env.NEXT_PUBLIC_SITE_URL) {
     return process.env.NEXT_PUBLIC_SITE_URL;
   }
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
+
+  if (process.env.URL) {
+    return process.env.URL;
   }
-  return process.env.NEXT_PUBLIC_SITE_URL || "";
+
+  if (process.env.DEPLOY_PRIME_URL) {
+    return process.env.DEPLOY_PRIME_URL;
+  }
+
+  return "http://localhost:3000";
 }
+
 export default getBaseUrl;
